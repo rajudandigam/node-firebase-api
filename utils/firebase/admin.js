56918f7ee023153324f6b63c13077b62f062fb6f
Raju Dandigam <raju0574@gmail.com>
2019-02-02 18:09:38 -0800

    feat: removed firbase setup file

--- utils/firebase/admin.js
+++ utils/firebase/admin.js
@@ -1,11 +1,9 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('../../secure_info/nodefirebaseapi-firebase-adminsdk.json');
 
-const fbAdminApp = admin.initializeApp({
+admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://nodefirebaseapi.firebaseio.com'
 });
-
-console.log('admin name ', fbAdminApp.name);
 
 module.exports = admin;
