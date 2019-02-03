a1fb3ea0d68791598c8de182dfba135e5edd9444
Raju Dandigam <raju0574@gmail.com>
2019-02-02 21:24:22 -0800

    feat: added firestore login logic

--- routes/login/login.js
+++ routes/login/login.js
@@ -1,11 +1,36 @@
 const { database: { realtime, firestore } } = require('../../config/config');
 
-const checkRealtime = (userRef, res) => {
-
+const checkRealtime = (userRef, res, password) => {
+  userRef.once('value', (snapshot) => {
+    if(snapshot.exists()) {
+      if(snapshot.val().password === password) {
+        res.json(`Welcome ${snapshot.key} Your logged in!!!`);
+      } else {
+        res.json('Credentials doesnt match');
+      }
+    } else {
+      res.json('Account doesnt exist');
+    }
+  });
 };
 
-const checkFirestore = (userRef, res) => {
-
+const checkFirestore = (userDoc, res, password) => {
+  userDoc.get()
+    .then( doc => {
+      if(!doc.exists) {
+        res.json('Account doesnt exist');
+      } else {
+        if(doc.data().password === password){
+          res.json(`Welcome ${doc.id} Your logged in!!!`);
+        } else {
+          res.json('Credentials doesnt match');
+        }
+      }
+    })
+    .catch( err => {
+      console.log('Getting user error ', err);
+      res.json('something went wrong. Please try again later');
+    })
 };
 
 const login = (req, res, admin) => {
@@ -14,26 +39,14 @@ const login = (req, res, admin) => {
   if(realtime) {
     const db = admin.database();
 
-    checkRealtime(db.ref(`users/${userName}`), res);
+    checkRealtime(db.ref(`users/${userName}`), res, password);
   }
 
   if(firestore) {
     const db = admin.firestore();
 
-    checkFirestore(db.collection('users').doc(userName), res);
+    checkFirestore(db.collection('users').doc(userName), res, password);
   }
-
-  db.ref(`users/${userName}`).once('value', (snapshot) => {
-    if(snapshot.exists()) {
-      if(snapshot.val().password === password) {
-        res.json(`Welcome ${userName} Your logged in!!!`);
-      } else {
-        res.json('Credentials doesnt match');
-      }
-    } else {
-      res.json('Account doesnt exist');
-    }
-  });
 };
 
 module.exports = login;
