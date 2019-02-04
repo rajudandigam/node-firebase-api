6e176d09cfdfb433e27d1d4169b28e5a1b2bda9b
Raju Dandigam <raju0574@gmail.com>
2019-02-03 16:29:40 -0800

    feat: updated login controller with async and await

--- routes/login/login.js
+++ routes/login/login.js
@@ -1,36 +1,39 @@
 const { database: { realtime, firestore } } = require('../../config/config');
 
-const checkRealtime = (userRef, res, password) => {
-  userRef.once('value', (snapshot) => {
-    if(snapshot.exists()) {
-      if(snapshot.val().password === password) {
-        res.json(`Welcome ${snapshot.key} Your logged in!!!`);
-      } else {
-        res.json('Credentials doesnt match');
-      }
+const checkRealtime = async (userRef, res, password) => {
+  const snapshot = userRef.once('value');
+
+  if(!snapshot) {
+    res.json('something went wrong. Please try again later');
+  }
+
+  if(snapshot.exists()) {
+    if(snapshot.val().password === password) {
+      res.json(`Welcome ${snapshot.key} Your logged in!!!`);
     } else {
-      res.json('Account doesnt exist');
+      res.json('Credentials doesnt match');
     }
-  });
+  } else {
+    res.json('Account doesnt exist');
+  }
 };
 
 const checkFirestore = (userDoc, res, password) => {
-  userDoc.get()
-    .then( doc => {
-      if(!doc.exists) {
-        res.json('Account doesnt exist');
-      } else {
-        if(doc.data().password === password){
-          res.json(`Welcome ${doc.id} Your logged in!!!`);
-        } else {
-          res.json('Credentials doesnt match');
-        }
-      }
-    })
-    .catch( err => {
-      console.log('Getting user error ', err);
-      res.json('something went wrong. Please try again later');
-    })
+  const doc = userDoc.get();
+
+  if(!doc) {
+    res.json('something went wrong. Please try again later');
+  }
+
+  if(!doc.exists) {
+    res.json('Account doesnt exist');
+  } else {
+    if(doc.data().password === password){
+      res.json(`Welcome ${doc.id} Your logged in!!!`);
+    } else {
+      res.json('Credentials doesnt match');
+    }
+  }
 };
 
 const login = (req, res, admin) => {
