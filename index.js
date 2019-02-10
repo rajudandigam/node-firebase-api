a1035ade150cf5051fd624f8573dd117863bcaa4
Raju Dandigam <raju0574@gmail.com>
2019-02-10 15:40:23 -0800

    feat: added morgan logger for the console log

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes/routes');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 app.use(router);
 
 const hostName = '127.0.0.1';
