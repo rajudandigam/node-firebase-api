ada96d9a8c5a5ad80493c18ee6fd7b5ee1b04e50
Raju Dandigam <raju0574@gmail.com>
2019-02-10 16:12:58 -0800

    feat: added json web token for generation

--- routes/routes.js
+++ routes/routes.js
@@ -2,19 +2,13 @@ const express = require('express');
 const signup = require('./signup/signup');
 const login = require('./login/login');
 const admin = require('../utils/firebase/admin');
+const authenticate = require('./authenticate/authenticate');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Welcome from api');
-});
-
-router.get('/login', (req, res) => {
-  login(req, res, admin);
-});
-
-router.post('/signup', (req, res) => {
-  signup(req, res, admin);
-});
+router.get('/', (req, res) => res.send('Welcome from api'));
+router.post('/authenticate', (req, res) => authenticate(req, res));
+router.get('/login', (req, res) => login(req, res, admin));
+router.post('/signup', (req, res) => signup(req, res, admin));
 
 module.exports = router;
